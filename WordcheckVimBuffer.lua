7b86f143f33d2f4fa971915f90bf5d6099a1b3d5
BG <gbgar@users.noreply.github.com>
2014-11-20 02:12:35 -0600

    Added license info and code annotations

--- WordcheckVimBuffer.lua
+++ WordcheckVimBuffer.lua
@@ -1,12 +1,30 @@
+-- WordcheckVimBuffer.lua
+-- 
+-- get default options and buffer name
 wcoptions = vim.eval('g:wordcheck_opts')
 file = b.fname
--- because vim.buffer().number returned as float,
--- returning an error on the vim side
+
+-- return bufnr with vim.buffer().number returned as float,
+-- producing an error on the vim side
 bnum = vim.command"call bufnr(\'.\')"
+
+-- get empty list from vim and run wordcheck
 wordpairlist = vim.eval('dictlist')
+
+-- The command'wordcheck -s vim..' produces lines of key-value pairs
+-- with each pair of lines corresponding to a pair of words
+-- matched by the program. 
 wordpairs = io.popen("wordcheck -s vim " .. wcoptions .. " -f " .. file)
+
+-- The following iterates through this output line by line,
+-- translating each (with some additions) into a dictionary 
+-- and producing a list of dictionaries corresponding to a
+-- list of matched words for use in setloclist().
 for line in wordpairs:lines() do
         pairentry = vim.dict()
+        -- pattern matching info:
+        -- http://www.lua.org/manual/5.2/manual.html#6.4.1
+        -- http://pgl.yoyo.org/luai/i/string.gmatch
         for x, y in string.gmatch(line, "(%w+)=\'([%w%s]*)\'") do
                 pairentry[x] = y
         end
--- wordcheck.vim
+++ wordcheck.vim
@@ -1,11 +1,20 @@
+" wordcheck.vim
+" For use with wordcheck redundancy checker:
+" https://github.com/gbgar/wordcheck
+" Blake Gardner
+" WTFPL 2014
+
+"quit if wordcheck is not installed
 if !executable("wordcheck")
         finish
 end
 
+"set default wordcheck_opts if none are provided by the user
 if !exists("g:wordcheck_opts")
         let g:wordcheck_opts = '-t line'
 endif
 
+"check entire buffer and set location list
 function! WordcheckBufferCheck()
         call setloclist(0,[])
         let dictlist = []
